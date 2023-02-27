840291963f437caffd5221a267b40263467f4533
Garik-Mk <garikmkrtchyan277353@gmail.com>
2023-02-27 17:06:26 +0400

    Added Windows functionality

--- kboard_training.py
+++ kboard_training.py
@@ -1,58 +1,39 @@
-from random import randint
+"""Simple blind typing trainer console app.
+
+Multiplatform functionality in progress.
+
+To quit the app, simply type ESC."""
+
 import os
+from random import randint
+from getch import Getch
 
 
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
 
-    def __call__(self): return self.impl()
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
-def gen_string(size = 10):
+def gen_string(size = 30):
+    """Generates random string containing lowercase
+      characters of English alphabet"""
     st = ''
     for i in range(size):
         st += chr(randint(ord('a'), ord('z')))
     return st
 
 
-def trainer(st):
-    for i, s in enumerate(st):
+def trainer_unix(st):
+    """Main trainer fucntion for Unix OS.
+    Clears the terminal, shows app a sequence that must
+    be typed in, replaces the letters that typed correctly with '*'
+    """
+    for i, elem in enumerate(st):
         while True:
             os.system('clear')
             print(st)
             os.system("stty -echo")
-            getch = _Getch()
+            getch = Getch()
 
-            if getch() == s:
+            inp = getch()
+
+            if inp == elem:
                 os.system("stty echo")
                 st = list(st)
                 st[i] = '*'
@@ -60,9 +41,39 @@ def trainer(st):
                 os.system('clear')
                 break
 
+            # if inp == 'q':        # TODO
+            #     os.system('clear')
+            #     quit()
+
+
+def trainer_win(st):
+    """Main trainer fucntion for Windows OS.
+    Clears the terminal, shows app a sequence that must
+    be typed in, replaces the letters that typed correctly with '*'"""
+    for i, elem in enumerate(st):
+        while True:
+            os.system('cls')
+            print(st)
+            getch = Getch()
+            
+            inp = getch()
+            if inp == bytes(elem, encoding='utf-8'):
+                st = list(st)
+                st[i] = '*'
+                st = ''.join(st)
+                break
+
+            if inp == b'\x1b':
+                os.system('cls')
+                quit()
+
+
+
+def main():
+    # trainer(gen_string())
+    trainer_win(gen_string())
+
+
+
 if __name__ == '__main__':
-    trainer(gen_string())
-
-
-
-
+    main()
